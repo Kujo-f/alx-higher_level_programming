e87a13ecd1188fad70f08318d763876e1646c74e
Bliss-treat <opiyokelly737@gmail.com>
2024-05-08 11:51:08 +0300

    added the first two files

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    prints a list of integers
+    
+    """
+    for int in my_list:
+        print("{:d}".format(int))
+    print()
